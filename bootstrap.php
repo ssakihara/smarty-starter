168645e81d98ad8023ea2547de3719232ce53951
SakiharaShinnosuke <sakihara.shinnosuke@gmail.com>
2020-08-18 13:08:45 +0900

    [update]phpファイルだけ読み込むようにする

--- bootstrap.php
+++ bootstrap.php
@@ -3,7 +3,7 @@
  * auto_prepend_file で最初に読み込むべきファイル
  */
 
-$bootstrap = glob(__DIR__ . '/bootstrap/*');
+$bootstrap = glob(__DIR__ . '/bootstrap/*.php');
 foreach ($bootstrap as $path) {
     require_once $path;
 }
