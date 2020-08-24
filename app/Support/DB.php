8014fbd2c061315881aa380b4a796f09e501e495
SakiharaShinnosuke <sakihara.shinnosuke@gmail.com>
2020-08-24 15:49:03 +0900

    [update]add db

--- app/Support/DB.php
+++ app/Support/DB.php
@@ -2,7 +2,28 @@
 
 namespace App\Support;
 
+use PDO;
+
 class DB
 {
-    //
+    static $dbh = null;
+
+    public static function boot(
+        $host = DB_HOST,
+        $dbname = DB_DATABASE,
+        $user = DB_USERNAME,
+        $password = DB_PASSWORD,
+        $port = DB_PORT
+    ) {
+        $dsn = "pgsql:dbname=${dbname} host=${host} port=${port}";
+        self::$dbh = new PDO($dsn, $user, $password);
+    }
+
+    public static function query($sql)
+    {
+        if (is_null(self::$dbh)) {
+            self::boot();
+        }
+        return self::$dbh->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
