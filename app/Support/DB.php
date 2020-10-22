f0a5447a45e223efe5ccb001b7fc200008331250
SakiharaShinnosuke <sakihara.shinnosuke@gmail.com>
2020-10-22 19:51:11 +0900

    [update]DB

--- app/Support/DB.php
+++ app/Support/DB.php
@@ -24,6 +24,38 @@ class DB
         if (self::$dbh === null) {
             self::boot();
         }
-        return self::$dbh->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+        return self::$dbh->query($sql);
+    }
+
+    public static function fetch($sql)
+    {
+        return self::query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public static function transaction(\Closure $func): void
+    {
+        if (self::$dbh === null) {
+            self::boot();
+        }
+
+        self::$dbh->beginTransaction();
+
+        try {
+            $func();
+            self::$dbh->commit();
+        } catch (\Exception $e) {
+            self::$dbh->rollBack();
+            error_log($e->getMessage());
+        }
+    }
+
+    public static function fetchColumns($table)
+    {
+        $result = [];
+        $tmp = self::fetch("SELECT column_name FROM information_schema.columns WHERE table_name = '${table}' ORDER BY ordinal_position");
+        $result = array_column($tmp, 'column_name');
+
+        return $result;
     }
 }
