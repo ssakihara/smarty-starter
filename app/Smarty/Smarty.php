9eef05498715c08611f42ce4e5cb9f8cffebbadd
SakiharaShinnosuke <sakihara.shinnosuke@gmail.com>
2020-10-23 17:41:33 +0900

    [update]smarty

--- app/Smarty/Smarty.php
+++ app/Smarty/Smarty.php
@@ -6,14 +6,21 @@ use Smarty as SmartyBase;
 
 class Smarty extends SmartyBase
 {
-    public function __construct(
-        $template_dir = __DIR__ . '/../../templates',
-        $compile_dir = __DIR__ . '/../../templates_c'
-    ) {
+    public function __construct()
+    {
         parent::__construct();
-        $this->template_dir = $template_dir;
-        $this->compile_dir = $compile_dir;
+        $this->caching = 0;
         $this->escape_html = true;
+    }
+
+    public function setCompileDir($dir): void
+    {
+        $this->compile_dir = $dir;
+    }
+
+    public function setPluginDir($dir): void
+    {
+        $this->addPluginsDir([$dir]);
     }
 
     public function display($template = null, $cache_id = null, $compile_id = null, $parent = null)
--- bootstrap/01_smarty.php
+++ bootstrap/01_smarty.php
@@ -3,3 +3,7 @@
 use App\Smarty\Smarty;
 
 $smarty = new Smarty();
+
+$smarty->setTemplateDir(PROJECT_ROOT . '/templates');
+$smarty->setCompileDir(PROJECT_ROOT . '/templates_c');
+$smarty->setPluginDir(PROJECT_ROOT . '/plugins/smarty');
