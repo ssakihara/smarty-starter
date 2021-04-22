5b450df01002a1b021ab0b1c5d2376ec01fc422f
SakiharaShinnosuke <sakihara.shinnosuke@gmail.com>
2021-04-22 16:58:12 +0900

    [update]色々アップデート

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,32 +1,32 @@
-const mix = require('laravel-mix')
+const mix = require('laravel-mix');
 
 mix.webpackConfig({
-    resolve: {
-        extensions: ['.js', '.scss', '.json'],
-    },
-    module: {
-        rules: [
-            {
-                enforce: 'pre',
-                test: /\.(js)?$/,
-                exclude: /node_modules/,
-                loader: 'eslint-loader',
-                options: {
-                    fix: true,
-                },
-            },
-        ],
-    },
-})
+  resolve: {
+    extensions: ['.js', '.scss', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js)?$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          fix: true,
+        },
+      },
+    ],
+  },
+});
 
 mix.options({
-    terser: {
-        terserOptions: {
-            compress: {
-                drop_console: true,
-            },
-        },
+  terser: {
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
     },
-})
+  },
+});
 
-mix.js('resources/js/app.js', 'assets/js').sass('resources/sass/app.scss', 'assets/css')
+mix.js('resources/js/app.js', 'assets/js').sass('resources/sass/app.scss', 'assets/css');
